2f25d5ed8393d982572322a3c6ba0e976a272008
Navyadubbaka <navyadubbaka9@gmail.com>
2026-03-17 14:21:46 +0530

    Added Github Actions of Java File

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+   
+        System.out.println("Hello, Navya !");
+         System.out.println("RollNo: 12345");
+          System.out.println("Branch: Computer Science");
+           System.out.println("College: CVR college of Engineering");
+    }
+}
